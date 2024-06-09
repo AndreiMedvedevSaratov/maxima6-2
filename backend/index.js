6131ca9fd193b2b0bd55501b7668940d6547699e
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-06-09 09:46:18 +0400

    server 2 step

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,9 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from server!");
+  res.send("Hello from server!!!!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+export default app;
